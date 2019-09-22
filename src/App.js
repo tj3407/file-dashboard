4fa833b8c1ea0f03dc88c7af9fdafd61f33f57eb
TJ Sornet <tsornet@gmail.com>
2019-09-22 10:10:44 -0700

    Added react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Layout from "./components/layout/Layout";
 import Sidebar from "./components/sidebar/Sidebar";
 import FileLayout from "./workflow/file-layout/FileLayout";
 import Header from "./components/header/Header";
+import { BrowserRouter, Route } from 'react-router-dom'
 
 function App() {
   const [mobileOpen, setMobileOpen] = React.useState(false);
@@ -16,8 +17,8 @@ function App() {
     setClickedItem(item);
   };
 
-  return (
-    <div className="App">
+  const layout = () => {
+    return (
       <Layout
         header={<Header handleDrawerToggle={handleDrawerToggle} />}
         sidebarComponent={
@@ -29,6 +30,25 @@ function App() {
         }
         mainContent={<FileLayout content={clickedItem} />}
       />
+    )
+  }
+
+  return (
+    <div className="App">
+      <BrowserRouter basename={window.location.pathname || ''}>
+        <Route exact path="/" component={layout} />
+      </BrowserRouter>
+      {/* <Layout
+        header={<Header handleDrawerToggle={handleDrawerToggle} />}
+        sidebarComponent={
+          <Sidebar
+            mobileOpen={mobileOpen}
+            handleDrawerToggle={handleDrawerToggle}
+            onItemClick={onItemClick}
+          />
+        }
+        mainContent={<FileLayout content={clickedItem} />}
+      /> */}
     </div>
   );
 }
